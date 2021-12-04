499813dbce84738d75dd14fe1b4def4ff01de335
Keith Zhang <Keith.Zhang@solace.com>
2021-12-04 08:29:10 -0500

    remove .history dir contents and add it to gitignore.

--- .history/resources/views/BookList.blade_20211203134329.php
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <title>Bootstrap Example</title>
-        <include>nclude ../public/header.pug</include>
--- .history/resources/views/BookList.blade_20211203134559.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <title>Bootstrap Example</title>
-        <include>user.blade.php</include>
-    </head>
-</html>
